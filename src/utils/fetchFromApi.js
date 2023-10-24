fea5f0dcf465ba68c23becad2422e7f3b51d9e98
Christopher Onyeneke <conyeneke1@gmail.com>
2023-10-24 19:58:56 +0100

    first commit

---
+++ src/utils/fetchFromApi.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const BASE_URL = "https://youtube-v31.p.rapidapi.com";
+
+const options = {
+  params: {
+    maxResults: "50",
+  },
+  headers: {
+    "X-RapidAPI-Key": "f8f318929bmsh20d46eaad43242cp138353jsn0fad41e7bc9f",
+    "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
+  },
+};
+
+export const fetchFromApi = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+  return data;
+};
